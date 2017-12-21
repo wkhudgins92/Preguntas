1146ab41856d6a5e471c8d21151eccc386376dd6
William Hudgins <will@wkhudgins.info>
2017-12-21 17:47:01 -0600

    Added more iteration values

--- maths/SquareRoot.py
+++ maths/SquareRoot.py
@@ -41,10 +41,15 @@ if __name__ == '__main__':
 
     # Test performance on all values
     tol = 0.00001
-    print "Testing on %s numbers with tolerance of %s" % (len(test), tol)
-    for i in range(len(test)):
-        num_wrong = 0
-        for num in test[i]:
-            if abs(square_root(num, tolerance = tol, max_iterations=1000) - math.sqrt(num)) >  tol:
-                num_wrong += 1
-        print "Square Root approximation got %s%% wrong with size %s." % ((num_wrong * 1.0 / len(test[i])) * 100, sizes[i])
+    print "This is an iterative method and its performance improves greatly with more iterations"
+    print ""
+
+    iterations = [100, 500, 1000, 5000]
+    for iters in iterations:
+        print "Testing on %s numbers with tolerance of %s and %s iterations" % (len(test), tol, iters)
+        for i in range(len(test)):
+            num_wrong = 0
+            for num in test[i]:
+                if abs(square_root(num, tolerance = tol, max_iterations=iters) - math.sqrt(num)) >  tol:
+                    num_wrong += 1
+            print "Square Root approximation got %s%% wrong with size %s." % ((num_wrong * 1.0 / len(test[i])) * 100, sizes[i])
